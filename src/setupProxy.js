8a5352fc87b5e00c06d1d6455b22b885cf42c0c1
VarshaHarishankara <91389434+VarshaHarishankara@users.noreply.github.com>
2022-06-09 11:20:16 -0700

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,21 +4,21 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://cors-everywhere.herokuapp.com/http://ec2-107-23-87-186.compute-1.amazonaws.com:8083',
+      target: 'https://cors-everywhere.herokuapp.com/http://ec2-3-238-79-254.compute-1.amazonaws.com:8083',
       changeOrigin: true,
     })
   );
   app.use(
     '/file-chunk',
     createProxyMiddleware({
-      target: 'https://cors-everywhere.herokuapp.com/http://ec2-107-23-87-186.compute-1.amazonaws.com:8082',
+      target: 'https://cors-everywhere.herokuapp.com/http://ec2-3-238-79-254.compute-1.amazonaws.com:8082',
       changeOrigin: true,
     })
   );
   app.use(
     '/protected/api',
     createProxyMiddleware({
-      target: 'https://cors-everywhere.herokuapp.com/http://ec2-107-23-87-186.compute-1.amazonaws.com:8080',
+      target: 'https://cors-everywhere.herokuapp.com/http://ec2-3-238-79-254.compute-1.amazonaws.com:8080',
       changeOrigin: true,
     })
   );
