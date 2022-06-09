feb3d60c4724df511af43f8f40caac83170fc853
VarshaHarishankara <91389434+VarshaHarishankara@users.noreply.github.com>
2022-06-09 11:04:45 -0700

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,21 +4,21 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http:// ec2-107-23-87-186.compute-1.amazonaws.com:8083',
+      target: 'https://cors-everywhere.herokuapp.com/http://ec2-107-23-87-186.compute-1.amazonaws.com:8083',
       changeOrigin: true,
     })
   );
   app.use(
     '/file-chunk',
     createProxyMiddleware({
-      target: 'http:// ec2-107-23-87-186.compute-1.amazonaws.com:8082',
+      target: 'https://cors-everywhere.herokuapp.com/http://ec2-107-23-87-186.compute-1.amazonaws.com:8082',
       changeOrigin: true,
     })
   );
   app.use(
     '/protected/api',
     createProxyMiddleware({
-      target: 'http:// ec2-107-23-87-186.compute-1.amazonaws.com:8080',
+      target: 'https://cors-everywhere.herokuapp.com/http://ec2-107-23-87-186.compute-1.amazonaws.com:8080',
       changeOrigin: true,
     })
   );
